18f34a69aca96caa7e50ff569daf458d4db80514
Walid Arghandewal <walid.argh@gmail.com>
2016-08-14 21:29:59 -0700

    getTabUrl

---
+++ popup.js
@@ -0,0 +1,14 @@
+function getTabUrl(callback) {
+	var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+  	var tab = tabs[0];
+  	var url = tab.url;
+  	console.assert(typeof url == 'string', 'tab.url should be a string');
+
+  	callback(url);
+  });
+}
