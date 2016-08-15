265a723578cdf3bb5bf7e806e8470797616bbf4c
Walid Arghandewal <walid.argh@gmail.com>
2016-08-15 15:02:25 -0700

    move comment section function working via content script

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
 function moveCommentSection() {
+	console.log('moving stuff');
+	debugger
 	var comments = document.getElementById("watch-discussion");
 	var sidebar = document.getElementById("watch7-sidebar-contents");
 	sidebar.insertBefore(comments, sidebar.firstChild);
 }
 
 function swapCommentsAndVideos() {
+	console.log('swapping stuff');
 	var comments = document.getElementById("comment-section-renderer");
 	var relatedVideos = document.getElementById("watch7-sidebar-modules");
 	var commentsParent = document.getElementById("watch-discussion");
@@ -17,4 +20,5 @@ document.addEventListener("DOMContentLoaded", function () {
 	console.log('hello');
 });
 
-swapCommentsAndVideos();
+// swapCommentsAndVideos();
+moveCommentSection();
