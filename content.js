1637a8f495112c3d00d43916461dda85e6b0790a
Walid Arghandewal <walid.argh@gmail.com>
2016-08-16 16:06:57 -0700

    html element for click listener on popup html

--- content.js
+++ content.js
@@ -49,5 +49,19 @@ function undoSwap() {
 	sidebar.style.padding = "";
 }
 
+function detectLoadMoreComments() {
+	var comments = document.getElementById("watch-discussion");
+	comments.scrollTop >= (comments.scrollHeight - comments.offsetHeight - 10);
+}
+
+
+
 document.addEventListener("spfdone", swapCommentsAndVideos);
 swapCommentsAndVideos();
+
+chrome.runtime.onMessage.addListener(function (request, sender, senderResponse) {
+	console.log("the message has been received" + senderResponse)
+});
+
+// TODO: fix issue comment section moving in theater mode
+/* Change comment section position to static in theater mode*/
