7d9bcc1d7b5db8364720b74b43d8d933dfcaaa29
Walid Arghandewal <walid.argh@gmail.com>
2016-08-16 13:41:41 -0700

    undo swap framework

--- content.js
+++ content.js
@@ -4,16 +4,16 @@ function moveCommentSection() {
 	sidebar.insertBefore(comments, sidebar.firstChild);
 }
 
-function swapCommentsAndVideos() {
-	var comments = document.getElementById("comment-section-renderer");
-	var relatedVideos = document.getElementById("watch7-sidebar-modules");
-	var commentsParent = document.getElementById("watch-discussion");
-	var relatedVideosParent = document.getElementById("watch7-sidebar-contents");
-	relatedVideosParent.insertBefore(comments, relatedVideosParent.firstChild);
-	commentsParent.insertBefore(relatedVideos, commentsParent.firstChild);
-}
+// function swapCommentsAndVideos() {
+// 	var comments = document.getElementById("comment-section-renderer");
+// 	var relatedVideos = document.getElementById("watch7-sidebar-modules");
+// 	var commentsParent = document.getElementById("watch-discussion");
+// 	var relatedVideosParent = document.getElementById("watch7-sidebar-contents");
+// 	relatedVideosParent.insertBefore(comments, relatedVideosParent.firstChild);
+// 	commentsParent.insertBefore(relatedVideos, commentsParent.firstChild);
+// }
 
-function swap() {
+function swapCommentsAndVideos() {
 	var comments = document.getElementById("watch-discussion");
 	var sidebar = document.getElementById("watch7-sidebar-contents");
 	var commentsParent = comments.parentElement;
@@ -31,10 +31,27 @@ function swap() {
 	sidebarHead.style.padding = "0px 0px 10px 5px";
 	var autoPlayBar = document.getElementsByClassName("autoplay-bar")[0];
 	autoPlayBar.style.position = "relative";
-	// sidebarHead.style.position = "relative";
-	// var autoPlayButton = document.getElementsByClassName("checkbox-on-off")[0];
-	// autoPlayButton.style.padding = "10px";
 }
 
-document.addEventListener("spfdone", swap);
-swap();
+function undoSwap() {
+	var comments = document.getElementById("watch-discussion");
+	var sidebar = document.getElementById("watch7-sidebar-contents");
+	var commentsParent = comments.parentElement;
+	var sidebarParent = sidebar.parentElement;
+	commentsParent.appendChild(comments);
+	sidebarParent.insertBefore(sidebar, sidebarParent.firstChild);
+	comments.style.height = "";
+	comments.style.width = "";
+	comments.style.overflowY = "";
+	comments.style.overflowX = "";
+	comments.style.position = "";
+	comments.style.marginTop = "";
+	sidebar.style.padding = "";
+	var sidebarHead = document.getElementsByClassName("watch-sidebar-head")[0];
+	sidebarHead.style.padding = "";
+	var autoPlayBar = document.getElementsByClassName("autoplay-bar")[0];
+	autoPlayBar.style.position = "";
+}
+
+document.addEventListener("spfdone", swapCommentsAndVideos);
+swapCommentsAndVideos();
