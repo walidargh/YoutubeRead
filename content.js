7be6691157b7cc381b63c4189aedb0b8efd9ec2c
Walid Arghandewal <walid.argh@gmail.com>
2016-08-18 14:12:42 -0700

    remove unused method

--- content.js
+++ content.js
@@ -1,11 +1,5 @@
 var currentSideBar = "relatedVideos";
 
-function moveCommentSection() {
-	var comments = document.getElementById("watch-discussion");
-	var sidebar = document.getElementById("watch7-sidebar-contents");
-	sidebar.insertBefore(comments, sidebar.firstChild);
-}
-
 function swapCommentsAndVideos() {
 	var comments = document.getElementById("watch-discussion");
 	var sidebar = document.getElementById("watch7-sidebar-contents");
